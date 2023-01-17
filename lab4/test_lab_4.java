0516276ba23b8945833977edd2a6d6742b655f4f
armzasan <arma7699@gmail.com>
2023-01-17 15:41:03 +0700

    lab4 test java

---
+++ lab4/test_lab_4.java
@@ -0,0 +1,22 @@
+package lab4;
+
+import java.util.Scanner;
+
+public class test_lab_4{
+    public static void main(String args[]){
+
+    Scanner scan= new Scanner(System.in);
+
+    //For string
+
+    String text= scan.nextLine();
+
+    System.out.println(text);
+
+    //for int
+
+    int num= scan.nextInt();
+
+    System.out.println(num);
+    }
+}
